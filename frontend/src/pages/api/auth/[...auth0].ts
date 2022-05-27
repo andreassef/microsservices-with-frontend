db09c091423889af30a48b578080b0a12e847691
andreassef <andreassef02@gmail.com>
2022-05-26 23:09:40 -0300

    estrutura inicial do ambiente

---
+++ frontend/src/pages/api/auth/[...auth0].ts
@@ -0,0 +1,3 @@
+import { handleAuth, UserProvider } from '@auth0/nextjs-auth0';
+
+export default handleAuth()
---
+++ purchases/src/http/test.resolver.ts
@@ -0,0 +1,12 @@
+import { UseGuards } from '@nestjs/common';
+import { Query, Resolver } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+
+@Resolver('test')
+export class TestResolver {
+  @Query(() => String)
+  @UseGuards(AuthorizationGuard)
+  hello() {
+    return 'Hello Graphql';
+  }
+}
